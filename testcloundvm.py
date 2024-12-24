636f9b1951233afa175acf25a52c5a154bd70611
Pattaratron <pattaratron@LAPTOP-JNKRJ038.>
2024-12-24 11:57:36 +0700

    test clound server

---
+++ testcloundvm.py
@@ -0,0 +1,17 @@
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t',"--time", default = 5)
+
+args = parser.parse_args()
+timet = int(args.time)
+print(timet)
+
+time.sleep(timet)
+
+input("Press Enter to coutinue...")
+
+time.sleep(timet)
+
+print('Bye')
